b8fb76273db2f4f5343d96e6d8dd1249402c0c5e
gd920129 <guodong000@gmail.com>
2013-08-17 14:38:44 +0800

    commit

--- src/com/guojin/whiteboard/WhiteBoardActivity.java
+++ src/com/guojin/whiteboard/WhiteBoardActivity.java
@@ -21,6 +21,8 @@ import com.guojin.entities.BoardEntity;
 
 public class WhiteBoardActivity extends Activity {
 	
+	private float oldDist = Float.NaN;	// 两触点之间的旧距离值
+	private PointF oldMidPoint;			// 旧两触点中心点
 	
 	private BoardEntity boardEntity = null;	// Board实体
 	
@@ -64,6 +66,14 @@ public class WhiteBoardActivity extends Activity {
 		
 		// 初始化控件
 		topbarLayout = (RelativeLayout)findViewById(R.id.topbar_layout);
+		topbarLayout.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
 		
 		// 模式选择
 		modeSelectGroup = (RadioGroup)findViewById(R.id.modesel_group);
@@ -176,7 +186,101 @@ public class WhiteBoardActivity extends Activity {
 		});
 	}
 	
-	public void setScaleText(String text) {
-		scaleTextView.setText(text);
+	/**
+	 * 获取两触点之间的距离
+	 * @param event
+	 * @return
+	 */
+	private float getPointsDist(MotionEvent event) {
+		float dx = event.getX(0) - event.getX(1);
+		float dy = event.getY(0) - event.getY(1);
+		return (float)Math.sqrt(dx * dx + dy * dy);
+	}
+	
+	/**
+	 * 获取两触点之间的中心点
+	 * @param event
+	 * @return
+	 */
+	private PointF getMidPoint(MotionEvent event) {
+		float x1 = event.getX(0);
+		float y1 = event.getY(0);
+		float x2 = event.getX(1);
+		float y2 = event.getY(1);
+		
+		int[] loc = new int[2];
+		boardView.getLocationOnScreen(loc);
+		
+		return new PointF((x1 + x2) / 2, (y1 + y2) / 2 - loc[1]);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		// 触点数目
+		int pointerCount = event.getPointerCount();
+		// 缩放比例
+		float scale = 1;
+		// 偏移距离
+		float dx = 0f;
+		float dy = 0f;
+		
+//		Log.d("DevLog", String.format("touch position: %f,%f", event.getX(), event.getY()));
+		
+		if (pointerCount == 2) {
+			switch (event.getActionMasked()) {
+			case MotionEvent.ACTION_MOVE:
+				
+				// 计算缩放比例scale
+				if (Float.isNaN(oldDist)) {
+					oldDist = getPointsDist(event);
+				} else {
+					float newDist = getPointsDist(event);
+					
+					if (Math.abs(newDist - oldDist) > 1f) {
+						scale = newDist / oldDist;
+						oldDist = newDist;
+					} 
+				}
+				
+				// 计算偏移距离
+				if (oldMidPoint == null) {
+					oldMidPoint = getMidPoint(event);
+				} else {
+					PointF newMidPoint = getMidPoint(event);
+					
+					float tdx = newMidPoint.x - oldMidPoint.x;
+					float tdy = newMidPoint.y - oldMidPoint.y;
+					
+					if (Math.abs(tdx) > 1f || Math.abs(tdy) > 1f) {
+						dx = tdx;
+						dy = tdy;
+						oldMidPoint = newMidPoint;
+					}
+				}
+				
+				PointF currMidPoint = getMidPoint(event);
+				boardEntity.calculate(currMidPoint.x, currMidPoint.y, scale
+						, dx, dy, boardView.getWidth(), boardView.getHeight());
+				// 显示缩放比例
+				scaleTextView.setText((int)(boardEntity.getTotalScale() * 100) + "%");
+				boardView.postInvalidate();
+				
+				
+//				Log.d("DevLog", String.format("Scale: %f\nDist: %f , %f", scale, dx, dy));
+				break;
+			case MotionEvent.ACTION_POINTER_UP:
+//				Log.d("DevLog", "Action up");
+				break;
+			case MotionEvent.ACTION_POINTER_DOWN:
+				oldDist = getPointsDist(event);
+				oldMidPoint = getMidPoint(event);
+				break;
+			}
+		} else {
+			boardEntity.onEntityTouchEvent(event);
+		}
+		
+		return false;
 	}
 }
