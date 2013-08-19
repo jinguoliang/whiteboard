bb34eb6cbda0f4e56a9471a75a8077d6631726ff
jinuxking <jinguol999@163.com>
2013-08-19 16:06:32 +0800

    橡皮,笔触擦除

--- src/com/guojin/entities/PathEntity.java
+++ src/com/guojin/entities/PathEntity.java
@@ -1,10 +1,12 @@
 package com.guojin.entities;
 
+import java.util.ArrayList;
+
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
+import android.graphics.PathMeasure;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.Region;
@@ -17,32 +19,47 @@ public class PathEntity implements Entity {
 	BoardEntity board;
 	Matrix mMatrix;
 	Paint mPaint;
+	Path mPath; 
 
 	double originalScale; // 存储
 	double boardX, boardY; // 存储
 	double currentScale; // 存储
-	Path mPath; // 存储
 	private float paintSize; // 存储
+	private ArrayList<PointF> pathPointsList;//存储
+	private int color;			//存储
+	
 	// 相对屏幕左上角的坐标
 	private float sx;
 	private float sy;
 
-	public PathEntity(BoardEntity b, Path path, Paint paint, float x, float y) {
+
+	public PathEntity(BoardEntity b, Path path, Paint paint, ArrayList<PointF>pointsList) {
 		this.board = b;
 		this.originalScale = b.getTotalScale();
 		this.currentScale = originalScale;
 
-		this.sx = x;
-		this.sy = y;
-		double xy[] = b.screenToBoardCoodTrans(x, y);
+		this.pathPointsList=pointsList;
+		this.sx = pointsList.get(0).x;
+		this.sy = pointsList.get(0).y;
+		double xy[] = b.screenToBoardCoodTrans(this.sx,this.sy);
 		this.boardX = xy[0];
 		this.boardY = xy[1];
 
 		this.mMatrix = new Matrix();
 		this.mPath = new Path(path);
 		this.mPaint = new Paint(paint);
+		this.color=mPaint.getColor();
 		this.paintSize = mPaint.getStrokeWidth();
 	}
+	
+	static PathEntity constructFromStr(String s){
+		
+		return null;
+	}
+	
+	public String toString() {
+			return null;
+	};
 
 	PointF tmpScreenPoint;
 	double tmpScale;
@@ -50,7 +67,6 @@ public class PathEntity implements Entity {
 	public void draw(Canvas canvas) {
 		tmpScale = board.getTotalScale();
 		if (Math.abs(currentScale - tmpScale) > 0) {
-			Log.e(TAG, "cs=" + currentScale + ":ts=" + tmpScale);
 			mMatrix.reset();
 			mMatrix.setScale((float) (tmpScale / currentScale),
 					(float) (tmpScale / currentScale), this.sx, this.sy);
@@ -105,15 +121,33 @@ public class PathEntity implements Entity {
 		// 空函数
 	}
 
-	public boolean containPoint(Point p) {
-		RectF bounds = new RectF();
-		mPath.computeBounds(bounds, true);
-		Region region = new Region();
-		region.setPath(mPath, new Region((int) bounds.left, (int) bounds.top,
-				(int) bounds.right, (int) bounds.bottom));
-		if (region.contains(p.x, p.y)) {
-			return true;
+	final float precision=5f;
+	public boolean containPoint(PointF point) {
+		ArrayList<PointF>list=getPointsArray();
+		Log.e(TAG,"list.size="+list.size());
+		for(PointF p:list){
+			float dist=distanceBetween(p, point);
+			if (dist-precision*2.5<0) {
+				return true;
+			}
 		}
 		return false;
 	}
+	private ArrayList<PointF> getPointsArray() {
+		ArrayList<PointF>list=new ArrayList<PointF>();
+		PathMeasure pm=new PathMeasure(mPath, false);
+		float []coords=null;
+		for(float  i=0+precision;i<=pm.getLength();i+=precision){
+			coords=new float[2];
+			pm.getPosTan(i, coords, null);
+			list.add(new PointF(coords[0],coords[1]));
+		}
+		return list;
+	}
+	
+	private float distanceBetween(PointF p1,PointF p2) {
+		return (float) Math.sqrt(Math.pow(p1.x-p2.x, 2)+Math.pow(p1.y-p2.y, 2));
+	}
+	
+	
 }
--- src/com/guojin/entities/PathFactory.java
+++ src/com/guojin/entities/PathFactory.java
@@ -7,15 +7,15 @@ import java.util.List;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
-import android.graphics.PorterDuff.Mode;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.PointF;
+import android.util.Log;
 import android.view.MotionEvent;
 
 public class PathFactory {
-	private static final float TOUCH_TOLERANCE = 5;
+	private static final float TOUCH_TOLERANCE =10f;
 	public static final int PATH_MODE_PAINT = 0x01;
 	public static final int PATH_MODE_ERASER = 0x02;
+	private static final String TAG = "PathFactory";
 
 	private int currentPathMode;
 
@@ -29,6 +29,8 @@ public class PathFactory {
 	private BoardEntity board;
 	private float px;
 	private float py;
+
+	private ArrayList<PointF> pathPoints;
 
 	public void changePathMode(int mode) {
 		this.currentPathMode = mode;
@@ -52,7 +54,7 @@ public class PathFactory {
 		mPaint.setStrokeCap(Paint.Cap.ROUND);
 		mPaint.setStrokeJoin(Paint.Join.ROUND);
 
-		changePathMode(PATH_MODE_ERASER);
+		changePathMode(PATH_MODE_PAINT);
 	}
 
 	public void draw(Canvas canvas) {
@@ -62,8 +64,12 @@ public class PathFactory {
 	}
 
 	private void touch_start(float x, float y) {
+		pathPoints = new ArrayList<PointF>();
 		cPath.reset();
 		cPath.moveTo(x, y);
+		if (currentPathMode != PathFactory.PATH_MODE_ERASER) {
+			pathPoints.add(new PointF(x, y));
+		}
 		px = sX = x;
 		py = sY = y;
 	}
@@ -75,6 +81,9 @@ public class PathFactory {
 		// 根据灵敏度，只有大于TOUCH_TOLERANCE的时候才在path添加一个点
 		if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
 			cPath.quadTo(sX, sY, (x + sX) / 2, (y + sY) / 2);
+			if (currentPathMode != PathFactory.PATH_MODE_ERASER) {
+				pathPoints.add(new PointF((x + sX) / 2, (y + sY) / 2));
+			}
 			sX = x;
 			sY = y;
 		}
@@ -84,7 +93,9 @@ public class PathFactory {
 		cPath.lineTo(sX, sY);
 		// 当抬起时，如果不是橡皮,一个笔触结束，于是，将其画到mBitmap上，并添加到pathList上
 		if (currentPathMode != PathFactory.PATH_MODE_ERASER) {
-			entityList.add(new PathEntity(this.board, cPath, mPaint, px, py));
+			pathPoints.add(new PointF(sX, sY));
+			entityList
+					.add(new PathEntity(this.board, cPath, mPaint, pathPoints));
 		}
 		// 重置cPath以便下一次重用
 		cPath.reset();
@@ -95,7 +106,7 @@ public class PathFactory {
 		float x = event.getX();
 		float y = event.getY();
 
-		//如果是清楚笔触模式,则根据触点位置查找笔触,删掉它
+		// 如果是清楚笔触模式,则根据触点位置查找笔触,删掉它
 		if (currentPathMode == PATH_MODE_ERASER) {
 			List<Entity> tmplist = new ArrayList<Entity>();
 			for (Iterator<Entity> iterator = this.entityList.iterator(); iterator
@@ -103,8 +114,7 @@ public class PathFactory {
 				Entity entity = (Entity) iterator.next();
 				if (entity.getType() == BoardEntity.TYPE_PATH_ENTITY) {
 
-					if (((PathEntity) entity).containPoint(new Point((int) x,
-							(int) y))) {
+					if (((PathEntity) entity).containPoint(new PointF(x, y))) {
 						tmplist.add(entity);
 					}
 				}
